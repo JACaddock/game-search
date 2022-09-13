a04724dc5995cbad2318046b7d08a179952ddad1
JACaddock <jamesacaddock@gmail.com>
2022-09-13 04:33:44 +0100

    Attempts to fix gh-pages yml

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <p>This is the beginning of the App</p>
       <a target="_blank" rel='noreferrer' href='https://google.co.uk'>Random link</a>
+      <p>Run Stuff</p>
     </div>
   );
 }
